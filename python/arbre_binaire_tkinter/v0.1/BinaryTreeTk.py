bf0a38eed0abe774ffd805ce6c87a377409dd53c
fdupont1 <dupontfrancois@free.fr>
2019-08-09 11:29:02 +0200

    maj

--- python/arbre_binaire_tkinter/v0.1/BinaryTreeTk.py
+++ python/arbre_binaire_tkinter/v0.1/BinaryTreeTk.py
@@ -14,67 +14,75 @@ class BinaryTreeTk(tk.Tk):
         tk.Tk.__init__(self, parent)
         self.parent = parent
         self.initialize()
-        
+
     def initialize(self):
-        self.geometry("%dx%d%+d%+d" % (800,370,150,100))
-        self.canvas = tk.Canvas(self, width=792, height = 320, bg = 'snow2')
+        self.geometry("%dx%d%+d%+d" % (800, 370, 150, 100))
+        self.canvas = tk.Canvas(self, width=792, height=320, bg='snow2')
         self.canvas.pack()
         self.node_position = self.calculate_node_position()
-        self.eteindre = tk.Button(self, text="Une action", command=self.action)
-        self.eteindre.pack()
+        self.action = tk.Button(self, text="Une action", command=self.action1)
+        self.action.pack()
+        self.walk = tk.Button(self, text="Parcours profondeur",
+                              command=self.walk)
+        self.walk.pack()
         self.tree = Tree(self.canvas, 0)
-        self.import_from_list([2, 1, 3, 4, 5, None, 6, None, None, 7, 8, None, None, 9, None, None, None, None, None, 10, None, 11, 12,  None, None, None, None, 13, 14])
+        self.import_from_list([2, 1, 3, 4, 5, None, 6, None, None, 7, 8, None,
+                               None, 9, None, None, None, None, None, 10, None,
+                               11, 12, None, None, None, None, 13, 14])
         self.tree.draw_tree(self.tree)
-        
-    def action(self):
+
+    def action1(self):
         #self.tree.draw_tree(self.tree)
         self.canvas.itemconfigure(self.tree.left.right.circle, fill="red")
         self.canvas.itemconfigure(self.tree.left.link_left, fill="red")
-    
-    def import_from_list(self, List, index = 0):
+
+    def import_from_list(self, List, index=0):
         if len(List) > 0:
             if index == 0:
                 self.tree.value = List[0]
                 self.tree.index = index
-                self.tree.x, self.tree.y = self.node_position[index][0], self.node_position[index][1]
-            fg = fd = None 
+                self.tree.x = self.node_position[index][0]
+                self.tree.y = self.node_position[index][1]
+            _fg = _fd = None
             if 2*index+1 < len(List):
-                fg = self.import_from_list(List, 2*index+1)
-                if 2*index+2 < len(List):    
-                    fd = self.import_from_list(List, 2*index+2)
+                _fg = self.import_from_list(List, 2*index+1)
+                if 2*index+2 < len(List):
+                    _fd = self.import_from_list(List, 2*index+2)
             if index == 0:
-                self.tree.left = fg
-                self.tree.right = fd
-            if List[index]!=None:
-                return Tree(self.canvas, List[index], fg ,fd, index, 
-                            self.node_position[index][0], 
+                self.tree.left = _fg
+                self.tree.right = _fd
+            if List[index] != None:
+                return Tree(self.canvas, List[index], _fg, _fd, index,
+                            self.node_position[index][0],
                             self.node_position[index][1])
             else:
-                return None    
-        
+                return None
+
     def calculate_node_position(self):
         node_position = []
-        for l in range(5):
-            decX = round(int(self.canvas['width'])/(2**(l+1)));
-            x = decX
-            y = 20+l*50
-            for n in range(1,2**l+1):
-                node_position.append([10+x, y]);
-                x += 2*decX
+        for _l in range(5):
+            dec_x = round(int(self.canvas['width'])/(2**(_l+1)))
+            _x = dec_x
+            _y = 20+_l*50
+            for _n in range(1, 2**_l+1):
+                node_position.append([10+_x, _y])
+                _x += 2*dec_x
         return node_position
-    
+
 class Tree(tk.Canvas):
-    
-    def __init__(self, canvas, value, left = None, right = None, index = None, 
-                 x = None, y = None):
+
+    def __init__(self, canvas, value, left=None, right=None, index=None,
+                 _x=None, _y=None):
         self.canvas = canvas
         self.value = value
         self.left = left
         self.right = right
         self.index = index
-        self.x, self.y = x, y
+        self.x, self.y = _x, _y
         self.radius = 12
-    
+        self.circle = None
+        self.text = None
+
     def draw_tree(self, tree):
         # parcours de l'arbre en profondeur pour tracer les liens et mettre
         # les noeuds dans une liste pour les dessiner à posteriori (par-dessus)
@@ -83,28 +91,32 @@ class Tree(tk.Canvas):
             # on dessine le sous-arbre gauche
             self.draw_tree(tree.left)
             # on dessine le lien entre l'arbre et son fils
-            tree.link_left = self.canvas.create_line(tree.x, tree.y, tree.left.x, tree.left.y,
-                        width = 2)
+            tree.link_left = self.canvas.create_line(tree.x, tree.y,
+                                                     tree.left.x, tree.left.y,
+                                                     width=2)
         #fils droit ?
         if tree.right:
             self.draw_tree(tree.right)
-            tree.link_right = self.canvas.create_line(tree.x, tree.y, tree.right.x, tree.right.y,
-                width = 2)
+            tree.link_right = self.canvas.create_line(tree.x, tree.y,
+                                                      tree.right.x, tree.right.y,
+                                                      width=2)
         if tree.index == 0:
             tree.draw_node()
         if tree.left:
             tree.left.draw_node()
         if tree.right:
             tree.right.draw_node()
-                
+
     def draw_node(self):
         # Création de polices
         font_value = tkinter.font.Font(family='Helvetica', size=10, weight='bold')
-        self.circle = self.canvas.create_oval(self.x-self.radius, self.y-self.radius, 
-                                self.x+self.radius, self.y+self.radius, fill="silver", width=2)
-        self.text = self.canvas.create_text(self.x, self.y, text=self.value, font=font_value)
+        self.circle = self.canvas.create_oval(self.x-self.radius, self.y-self.radius,
+                                              self.x+self.radius, self.y+self.radius,
+                                              fill="silver", width=2)
+        self.text = self.canvas.create_text(self.x, self.y, text=self.value,
+                                            font=font_value)
         print(self.circle)
-        
+
 
 bt = BinaryTreeTk(None)
 bt.mainloop()
